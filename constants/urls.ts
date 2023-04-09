fa0c3587205a14137cd4e23866b52c290099d5e7
Kim Dong Hun <hunkim98@gmail.com>
2023-04-09 19:00:22 +0900

    Update urls

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,2 +1,2 @@
-export const DIFFUSION_URL = "/genai" + "/txt2img";
-export const BG_REMOVE_URL = "/genai" + "/background_remover";
+export const DIFFUSION_URL = "genai" + "/txt2img";
+export const BG_REMOVE_URL = "genai" + "/background_remover";
